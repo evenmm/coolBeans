c29a7526c0ae6ac9c0000063dd583d28fbed96b9
Haris Fawad <haris.fawad@medisin.uio.no>
2021-03-17 10:31:22 +0100

    Implemented add and test_add

---
+++ example.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert (add(0.1, 0.2) - 0.3) < 1e-6
+    assert add('space', 'ship') == 'spaceship'
+
+def substract(a, b):
+    return a + b
+
+#def test_substract():
+#    assert substract(2, 3) == -1
+
+
